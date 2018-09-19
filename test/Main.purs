3d8bc6027a4de5f615a1e41d3717cc8c9f4a3ed8
Liam Goodacre <goodacre.liam@gmail.com>
2018-09-19 21:20:58 +0100

    Add UnsafeRefEq and UnsafeRefEqFallback newtypes with Eq instances

--- test/Main.purs
+++ test/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Effect (Effect)
 import Effect.Exception (throwException, error)
 
-import Unsafe.Reference (unsafeRefEq, reallyUnsafeRefEq)
+import Unsafe.Reference (unsafeRefEq, reallyUnsafeRefEq, UnsafeRefEq(..), UnsafeRefEqFallback(..))
 
 data Foo = Foo String
 data Bar = Bar String
@@ -16,6 +16,10 @@ newtype Bar' = Bar' String
 assert :: Boolean -> String -> Effect Unit
 assert true _ = pure unit
 assert _ desc = throwException (error desc)
+
+data X = Y Unit | Z
+
+derive instance eqX :: Eq X
 
 main :: Effect Unit
 main = do
@@ -32,3 +36,7 @@ main = do
   assert (unsafeRefEq foo3 foo4) "unsafeRefEq newtype"
   assert (reallyUnsafeRefEq foo3 bar2) "reallyUnsafeRefEq newtype"
   assert (not reallyUnsafeRefEq foo1 bar1) "not reqallyUnsafeRefEq data"
+  assert (UnsafeRefEq 0 == UnsafeRefEq 0) "UnsafeRefEq eq with identical values"
+  assert (UnsafeRefEq (Y unit) /= UnsafeRefEq (Y unit)) "UnsafeRefEq not eq with non-identical values"
+  assert (UnsafeRefEqFallback (Y unit) == UnsafeRefEqFallback (Y unit)) "UnsafeRefEqFallback eq with non-identical but equal values"
+  assert (UnsafeRefEqFallback (Y unit) /= UnsafeRefEqFallback Z) "UnsafeRefEqFallback not eq with non equal values"
